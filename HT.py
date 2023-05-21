b6e26c0c517f483d8c6526f1f8884fe574257b12
David <david.daso@sterling.ng>
2023-05-21 10:54:35 +0100

    two_sum

--- HT.py
+++ HT.py
@@ -91,7 +91,19 @@ def group_anagrams(strings):
     return list(anagram_group.values())
 
 
-
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
+    
+    
+    
+    
+print ( two_sum([2, 7, 11, 15], 9) )
 
 # HT = HashTable()
 
